18635e003877423052322da1d723c2a1e1b554d3
dako0819 <daniil.konstantinov@netcracker.com>
2020-08-08 15:54:07 +0300

    add models inheritance

--- courses/models.py
+++ courses/models.py
@@ -47,7 +47,15 @@ class Content(models.Model):
                                related_name='contents',
                                on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType,
-                                     on_delete=models.CASCADE)
+                                     on_delete=models.CASCADE,
+                                     limit_choices_to={
+                                         'model__in': (
+                                             'text', 'video',
+                                             'image', 'file'
+                                         )})
+    # Мы добавили атрибут limit_choices_to, чтобы ограничить типы содержимого ContentType, которые могут участвовать
+    # в связи. Чтобы фильтровать объекты ContentType при запросах,
+    # указали условие model__in и значения 'text', 'video', 'image' и 'file'.
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
 #     Это модель Content. Модуль курса может содержать множество объектов этого типа, поэтому мы используем ForeignKey
@@ -58,3 +66,44 @@ class Content(models.Model):
 # ...+ item – поле типа GenericForeignKey, которое обобщает данные из предыдущих двух.
 # Только поля content_type и object_id будут представлены в базе данных соответствующими столбцами.
 # Поле item используется только в Python-коде и позволяет вам получить или задать связанный объект.
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User,
+                              related_name='%(class)s_related',
+                              on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+# В этом фрагменте вы создали абстрактную модель ItemBase, задав в опциях класса Meta атрибут abstract=True.
+# Она содержит четыре поля: owner, title, created и updated, – которые будут общими для всех дочерних моделей.
+# Поле owner содержит данные пользователя, который создал объект. Так как в
+# дочерних классах будет присутствовать это поле, необходимо задать related_name
+# для каждого из них.
+# Django задает связанное наименование related_name в виде %(class)s,
+# но мы определили его как '%(class)s_related'. Таким образом, объекты каждой
+# дочерней модели будут доступны по именам
+# text_related, file_related, image_related и video_related.
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+
+
+class Video(ItemBase):
+    url = models.URLField()
+    # Мы применили поле URLField, чтобы сохранять URL видео для его скачивания.
